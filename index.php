0df2b9810ff7660e29562e7b61c6ce6e6e1d618f
valerio <valerio.deprosperi@gmail.com>
2020-10-22 17:01:57 +0200

    public function user 1,2

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
-class User{
+class User {
 public $nome;
 public $cognome;
 public $email;
 
+public function _construct($_nome, $_cognome, $_email)
+{
+$this->nome = $_nome;
+$this->cognome = $_cognome;
+$this->email = $_email;
 
 
 }
 
+
+}
+
+$user1 = new User()
+$user2 = new User()
 
  ?>
