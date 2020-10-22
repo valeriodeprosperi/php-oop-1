d6255ad4decfbe62d758f7b625e3b0fc059cb770
valerio <valerio.deprosperi@gmail.com>
2020-10-22 17:08:26 +0200

    init table

--- index.php
+++ index.php
@@ -22,3 +22,22 @@ $user2 = new User("giorgio", "rossi", "giorgio.rossi@gmail.com");
 $users = [$user1, $user2];
 
  ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Users</title>
+  </head>
+  <body>
+    <table>
+      <thead>
+        <td>nome</td>
+        <td>cognome</td>
+        <td>email</td>
+
+      </thead>
+    </table>
+
+  </body>
+</html>
